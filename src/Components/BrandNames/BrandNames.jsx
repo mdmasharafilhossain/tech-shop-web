e9ecb43b68af7375ec1d197fa8ad3d35f6e1e845
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2023-10-18 22:44:07 +0600

    json data added

---
+++ src/Components/BrandNames/BrandNames.jsx
@@ -0,0 +1,11 @@
+
+
+const BrandNames = () => {
+    return (
+        <div className="pt-16 bg-gray-100">
+           <h2 className="font-bold border bg-green-100 text-green-500  text-5xl text-center mt-16 py-5 mb-10">Popular Brand Products</h2> 
+        </div>
+    );
+};
+
+export default BrandNames;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../Banner/Banner";
+import BrandNames from "../BrandNames/BrandNames";
 
 
 const Home = () => {
     return (
         <div>
            <Banner></Banner>
+           <BrandNames></BrandNames>
         </div>
     );
 };

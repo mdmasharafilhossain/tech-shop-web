c7fbd7c6275ae340a91c644e58e9530e0e052e30
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2023-10-19 22:35:15 +0600

    footer section added

---
+++ src/Components/FireBase/FireBase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWtkM5k-biqYBexdIUZXci6vrODwLs0FI",
+  authDomain: "brand-shop-a6c5c.firebaseapp.com",
+  projectId: "brand-shop-a6c5c",
+  storageBucket: "brand-shop-a6c5c.appspot.com",
+  messagingSenderId: "203378561659",
+  appId: "1:203378561659:web:3962dc40f3055f5caf08de"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
